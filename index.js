7789cc27e2e353e0b7efd892905dcde504cf8b7f
Spacey_Core <59103833+Exaby@users.noreply.github.com>
2022-04-19 13:09:47 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+var http = require("http");
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(
+      '<html lang="en">' +
+        "<head>" +
+        '<meta http-equiv="content-type" content="text/html; charset=UTF-8">' +
+        '<meta charset="UTF-8">' +
+        '<meta name="viewport" content="width=device-width; height=device-height;"></meta>' +
+        '<link rel="shortcut icon" href="https://cdn.spco.xyz/assets/img/home/favicon.png"></link>' +
+        "<title>" +
+        req.url.substring(1) +
+        "</title>" +
+        '<link rel="stylesheet" href="https://spco.xyz/video/c/videocontrols.css" />' +
+        "<link" +
+        '  rel="stylesheet"' +
+        '  href="https://spco.xyz/video/c/TopLevelVideoDocument.css"' +
+        "/>" +
+        "<link" +
+        '  rel="stylesheet"' +
+        '  href="https://spco.xyz/video/c/BottomLevelVideoDocument.css"' +
+        "/>" +
+        "<script" +
+        '  type="text/javascript"' +
+        '  src="https://spco.xyz/video/j/TopLevelVideoDocument.js"' +
+        "></script>" +
+        "<meta" +
+        '  name="description"' +
+        '  content="HTML5 Video Player From Mozilla On Every Browser"' +
+        "/>" +
+        '<meta property="og:type" content="video.other" />' +
+        '<meta property="og:video:width" content="1280" />' +
+        '<meta property="og:video:height" content="720" />' +
+        '<meta property="og:video:url" content="' +
+        req.url.substring(1) +
+        '" />' +
+        "</head>" +
+        "<body>" +
+        '<video controls="controls" autoplay="autoplay" loop="" src="' +
+        req.url.substring(1) +
+        '" poster="" height="100%%"></video>' +
+        "</body>" +
+        "</html>"
+    );
+    res.end();
+  })
+  .listen(8080);
